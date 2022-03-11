1c6d7cbbb658cf3c482c4da3d71363afb55cd5d6
= <=>
2022-03-11 10:45:35 +0530

    all done

---
+++ include/config.php
@@ -0,0 +1,13 @@
+
+<?php
+error_reporting(0);
+$dbcon = mysqli_connect("localhost","root","","nspsjadugora_db");
+
+// if($dbcon->connect_error)
+// {
+//     die("Connection Wrong: " .$dbcon->connect_error);
+// }
+
+?>
+
+
